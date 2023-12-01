d12c2c1bcc646f34a8d781b4a47870ba8a87b861
angeljimenez91 <139405887+angeljimenez91@users.noreply.github.com>
2023-12-01 19:50:15 +0000

    primercard

--- src/app.js
+++ src/app.js
@@ -7,5 +7,43 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function generadorNumero() {
+    let valores = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A"
+    ];
+    let valoresrandom = Math.floor(Math.random() * valores.length);
+    return valores[valoresrandom];
+  }
+
+  function generadorSimbolo() {
+    let simbolo = ["♥", "♦", "♣", "♠"];
+    let simbolorandom = Math.floor(Math.random() * simbolo.length);
+    return simbolo[simbolorandom];
+  }
+  let simboloRandom = generadorSimbolo();
+  document.querySelector("#top").innerHTML = simboloRandom;
+  document.querySelector("#bottom").innerHTML = simboloRandom;
+  document.querySelector("#cardNumero").innerHTML = generadorNumero();
+
+  if (simboloRandom == "♥" || simboloRandom == "♦") {
+    document.getElementById("top").style.color = "red";
+    document.getElementById("bottom").style.color = "red";
+  } else {
+    document.getElementById("top").style.color = "black";
+    document.getElementById("bottom").style.color = "black";
+  }
+  console.log(simboloRandom);
+  console.log(generadorNumero());
 };
